d3d5ce28e6e46dd6f492d9681ae8b7749c7fa6d0
cdc3715 <464278398@qq.com>
2017-01-06 15:17:57 +0800

    初始化上传

---
+++ GIT-MAVEN-FRONT/src/main/java/com/Add_Test.java
@@ -0,0 +1,10 @@
+package com;
+
+public class Add_Test {
+	int a = 5;
+	int b = 6;
+	
+	public int add1(int c){
+		return a+b+c;
+	}
+}
---
+++ GIT-MAVEN-FRONT/src/main/java/com/C_P_Model_Test.java
@@ -0,0 +1,12 @@
+package com;
+
+import com.model.User;
+
+public class C_P_Model_Test {
+	
+	public static void main(String[] args) {
+		User user = new User();
+		user.setName("fuck java");
+		System.out.println(user.getName());
+	}
+}
